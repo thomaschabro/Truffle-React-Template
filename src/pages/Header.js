4e9520822d0353a025167296a372b254f6f0c935
pauloschade <paulosc4@al.insper.edu.br>
2022-03-22 15:10:58 -0300

    css

---
+++ src/pages/Header.js
@@ -0,0 +1,17 @@
+import './main.css';
+const Head = () => {
+    return (
+    <div>
+        <header className='header'>
+        <div className='img'>
+            <img src ={"logoBI.png"} />
+        </div>
+        <h1>
+            Template Truffle React
+        </h1>
+        </header>
+    </div>
+    )
+}
+
+export default Head;
